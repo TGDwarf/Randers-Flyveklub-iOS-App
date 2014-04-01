5f88b79dbe1c69a6832054d4890774f0fdb15eb3
madstorp <madstorp1337@gmail.com>
2014-04-01 18:58:54 +0200

    tilføjet kontakt date til secound view controller

--- Randers Flyveklub iOS App/SecondViewController.m
+++ Randers Flyveklub iOS App/SecondViewController.m
@@ -9,15 +9,21 @@
 #import "SecondViewController.h"
 
 @interface SecondViewController ()
-
+@property (strong, nonatomic) IBOutletCollection(UITableView) NSArray *contakt;
 @end
-
 @implementation SecondViewController
+
+@synthesize tableData;
+
+
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+tableData = [[NSArray alloc] initWithObjects:@"TG Drowf",@"Satans NIsser",@"JesperB21",@"", nil];
+
+[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +31,28 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [tableData count];
+}
+
+-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = nil;
+    
+    cell = [tableView dequeueReusableCellWithIdentifier:@"Mycell"];
+            
+            if (cell == nil)
+    {
+        cell =[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"MyCell" ];
+    
+    
+    }
+    
+    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    return cell;
+}
+
 
 @end
